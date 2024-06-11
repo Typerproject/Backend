6eecc3e9729535c11e0806d53e4f3389e0cb930c
lemonticsoul <sjho714@naver.com>
2024-06-11 14:28:54 +0900

    ✨feat:재무재표 api router추가

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const reportRouter=require('./routes/report');
+const finaceRouter=require('./routes/finance');
+
 const cors=require("cors");
 
 var app = express();
@@ -26,6 +28,7 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/report',reportRouter);
+app.use('/finance',finaceRouter);
 
 //mongodb연결을위해 비밀번호 가림
 const dotenv = require("dotenv");
