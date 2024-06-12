2d5c79f3b21aee54906103fc23bdea794c542ee9
kim nahyeon <stome6322@inha.edu>
2024-06-12 11:29:24 +0900

    ✨ feat: newsCrawler api 추가

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const reportRouter=require('./routes/report');
 const finaceRouter=require('./routes/finance');
+const newsRouter = require('./routes/news');
 
 const cors=require("cors");
 
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/report',reportRouter);
 app.use('/finance',finaceRouter);
+app.use('/news', newsRouter);
 
 //mongodb연결을위해 비밀번호 가림
 const dotenv = require("dotenv");
---
+++ backend/routes/news.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { newsCrawler } = require("../../crawler/newsCrawler");
+const validUrl = require('valid-url');
+
+router.post("/", async function (req, res) {
+  try {
+    const { url } = req.body;
+    // console.log(url);
+
+    if (!validUrl.isWebUri(url)) {
+      return res.status(400).json({
+        message: "잘못된 url 형식입니다.",
+      });
+    }
+
+    const data = await newsCrawler(url);
+    // console.log(data);
+
+    return res.status(200).json({
+      data,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "server error",
+    });
+  }
+});
+
+module.exports = router;
